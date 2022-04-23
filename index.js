d0eb84b82edac934dc6fb9cbb76b764e8014d0f3
Travis Arnold <ftntravis@gmail.com>
2022-04-23 14:26:54 -0700

    fix undefined value

--- index.js
+++ index.js
@@ -8,27 +8,28 @@ process.argv.slice(2).forEach((arg) => {
   }
 
   let [key, value] = arg.split('=')
-  const isNumber = !isNaN(value)
-  const isFloat = value.includes('.')
-
-  if (isNumber) {
-    if (isFloat) {
-      value = parseFloat(value)
-    } else {
-      value = Number(value)
-    }
-  }
-
-  if (value === 'true') {
-    value = true
-  }
-
-  if (value === 'false') {
-    value = false
-  }
 
   if (value === undefined) {
     value = true
+  } else {
+    const isNumber = !isNaN(value)
+    const isFloat = value.includes('.')
+
+    if (isNumber) {
+      if (isFloat) {
+        value = parseFloat(value)
+      } else {
+        value = Number(value)
+      }
+    }
+
+    if (value === 'true') {
+      value = true
+    }
+
+    if (value === 'false') {
+      value = false
+    }
   }
 
   flags[key.slice(2)] = value
